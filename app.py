0f7409e31f8a6a72deae936a7e1212402af46676
syedmusharaf0 <mushumushu656@gmail.com>
2026-03-31 16:37:24 +0300

    Initial Streamlit app

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# Title
+st.title("Hello, I'm Musharaf 👋")
+
+# Subtitle
+st.subheader("Welcome to my first Streamlit App 🚀")
+
+# Text
+st.write("This is my beginner DevOps + Streamlit project.")
+
+# Input box
+name = st.text_input("Enter your name:")
+
+# Button
+if st.button("Say Hello"):
+    if name:
+        st.success(f"Hello {name}! 👋 Welcome to Musharaf's app.")
+    else:
+        st.warning("Please enter your name.")
+
+# Sidebar
+st.sidebar.title("About Me")
+st.sidebar.write("Hi, I'm Musharaf — Learning DevOps & Data Science 🚀")
