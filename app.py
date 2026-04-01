52ceb636d36572d4fb51f4d09d47f6b3837b6bd8
syedmusharaf0 <mushumushu656@gmail.com>
2026-04-01 14:19:10 +0300

    trigger Pipeline

--- app.py
+++ app.py
@@ -25,3 +25,5 @@ st.sidebar.write("Hi, I'm Musharaf — Learning DevOps & Data Science 🚀")
 st.sidebar.write("MY EAGER TO CONTRIBUTE TO DEVOPS")
 st.sidebar.write("hello learning devops")
 st.sidebar.write("CI/CD")
+
+print("CI/CD test")
